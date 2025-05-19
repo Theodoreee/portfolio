81146a1edced5b765c69468999ecd36af4a910f0
theodore.vaunois@epita.fr <theodore.vaunois@epita.fr>
2025-05-19 11:17:34 +0200

    update stat

--- src/components/ResultsPanel.vue
+++ src/components/ResultsPanel.vue
@@ -17,15 +17,15 @@ export default {
     data() {
         return {
             results: [
-                { videoUrl: "https://www.youtube.com/embed/GEFETK2g7xY", views: "161 000" },
+                { videoUrl: "https://www.youtube.com/embed/GEFETK2g7xY", views: "175 000" },
                 { videoUrl: "https://www.youtube.com/embed/_CiwpCcPtPg", views: "33 000" },
-                { videoUrl: "https://www.youtube.com/embed/k_vYxsSPQMs", views: "187 000" },
+                { videoUrl: "https://www.youtube.com/embed/k_vYxsSPQMs", views: "238 000" },
                 { videoUrl: "https://www.youtube.com/embed/KVhPlxs4Nmg", views: "11 000" },
                 { videoUrl: "https://www.youtube.com/embed/d1BCri2fVDs", views: "105 000" },
-                { videoUrl: "https://www.youtube.com/embed/Do7F75pocMk", views: "52 000" },
+                { videoUrl: "https://www.youtube.com/embed/Do7F75pocMk", views: "54 000" },
                 { videoUrl: "https://www.youtube.com/embed/fa4QrbXlsi0", views: "140 000" },
                 { videoUrl: "https://www.youtube.com/embed/VZhhVNe6G5g", views: "24 000" },
-                { videoUrl: "https://www.youtube.com/embed/6-xazmcTrZ8", views: "105 000" },
+                { videoUrl: "https://www.youtube.com/embed/6-xazmcTrZ8", views: "172 000" },
             ]
         };
     }
